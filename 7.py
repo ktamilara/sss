49a92b96a786073c9f7b087284e7d62ba77c71c6
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-13 14:40:02 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,7 +1,7 @@
 n1=int(input())
 l1=list(map(int,input().split()))
 for i1 in range(0,len(l1)):
-    if(l1[i1]%2==0 and 1i%2!=0):
+    if(l1[i1]%2==0 and i1%2!=0):
             print(l1[i1],end=" ")
     elif(l1[i1]%2!=0 and i1%2==0):
             print(l1[i1],end=" ")
