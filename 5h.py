be93eae4f76441b272201a59245f35bdb15c8257
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-13 14:21:36 +0530

    Update 5h.py

--- 5h.py
+++ 5h.py
@@ -2,9 +2,9 @@ b1=int(input())
 sb1=str(b1)
 q1=0
 for i1 in range(0,len(sb1)):
-    if int(sb1[i1:i1+2])<26 and len(str(int(sb1[i1:1i+2])))==2:
+    if int(sb1[i1:i1+2])<26 and len(str(int(sb1[i1:i1+2])))==2:
         q1+=1
 if q1==2:
-    print(q1+1q//2)
+    print(q1+q1//2)
 else:
     print(q1+q1//2+1)
