9f8de6afef43816cf79f0b8089602de6b4f8d990
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-13 15:35:50 +0530

    Update 68.py

--- 68.py
+++ 68.py
@@ -1,7 +1,7 @@
-number11,number12=input().split()
-if(number11.isdigit() and number12.isdigit()):
-              number11,numbe1r2=int(number11),int(number12)
-              if(number12==1):
+x11,x12=input().split()
+if(x11.isdigit() and x12.isdigit()):
+              x11,x12=int(x11),int(x12)
+              if(x12==1):
                   print("1 2")
               else:
-                 print("1 "+str(number11-number12))
+                 print("1 "+str(x11-x12))
