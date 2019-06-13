03d14b09b3d9d152ba2dc20ed780325cc438076b
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-13 14:39:48 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,7 +1,7 @@
-l1=int(input())
-arr1=list(map(int,input().split()))
-for i1 in range(0,len(arr1)):
-    if(arr1[i1]%2==0 and i1%2!=0):
-        print(arr1[i1],end=" ")
-    elif(arr[i1]%2!=0 and i1%2==0):
-        print(ar1r[i1],end=" ")
+n1=int(input())
+l1=list(map(int,input().split()))
+for i1 in range(0,len(l1)):
+    if(l1[i1]%2==0 and 1i%2!=0):
+            print(l1[i1],end=" ")
+    elif(l1[i1]%2!=0 and i1%2==0):
+            print(l1[i1],end=" ")
