487b227e0325381c6c2470ed6a5b71acfc06f47e
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-13 14:45:00 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,10 +1,10 @@
-n11=int(input())
-li1=list(map(int,input().split()))
-op1=[]
-for i1 in range(0,len(li1)-2):
-  for j1 in range(i1+1,len(li1)-1):
-    for k1 in range(j1+1,len(li1)):
-      if li1[i1]+li1[j1]==li1[k1]:
-        op.append([li1[i1],li1[j1],li1[k1]])
+n1=int(input())
+l1=list(map(int,input().split()))
+o1p=[]
+for i1 in range(0,len(l1)-2):
+  for j1 in range(i1+1,len(l1)-1):
+    for k1 in range(j1+1,len(l1)):
+      if l1[i1]+l1[j1]==l1[k1]:
+        op1.append([l1[i1],l1[j1],l1[k1]])
 for i1 in op1:
   print(*i1)
