5f4b8777bb45039e43dc4474ed6d48560062d945
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-13 15:43:07 +0530

    Update 65.py

--- 65.py
+++ 65.py
@@ -1,7 +1,7 @@
-number11,number12 = map(int,input().split())
-number13 = list(map(int,input().split()))
+num11,num12 = map(int,input().split())
+num13 = list(map(int,input().split()))
 amount1 = int(input())
-total1 = (sum(number13)-number13[number12])//2
+total1 = (sum(num13)-num13[num12])//2
 if amount1 == total1:
   print("Bon Appetit")
 else:
