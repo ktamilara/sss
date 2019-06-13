f920ce1db5433d76231713891d14f1b14df9a900
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-13 15:33:04 +0530

    Create 68.py

---
+++ 68.py
@@ -0,0 +1,7 @@
+number11,number12=input().split()
+if(number11.isdigit() and number12.isdigit()):
+              number11,numbe1r2=int(number11),int(number12)
+              if(number12==1):
+                  print("1 2")
+              else:
+                 print("1 "+str(number11-number12))
